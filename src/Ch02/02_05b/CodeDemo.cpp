0b9004b157cd86f0cc82a37589f5b1a7b9b25970
rida-hashmi <rida.f.hashmi@icloud.com>
2025-01-10 22:01:20 +0000

    using variables

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,7 +4,24 @@
 
 #include <iostream>
 
+int a,b = 5;
+
 int main(){
+    bool myFlag; 
+    a = 7;
+    myFlag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << myFlag << std::endl;
+    myFlag = true;
+    std::cout << "flag = " << myFlag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "(unsigned) b - a = " << positive << std::endl;
+
     
     std::cout << std::endl << std::endl;
     return 0;
