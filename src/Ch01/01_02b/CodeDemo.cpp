8deb2fcfd86889df14dcaa7ba9c924f86f933c2e
rida-hashmi <rida.f.hashmi@icloud.com>
2025-01-10 21:11:57 +0000

    created a hellow world statement

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    std::cout << std::endl <<std::endl; //prints 2 empty lines for clarity
+    return 0;
+
+}
